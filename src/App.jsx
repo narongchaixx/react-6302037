102c5f2b06a0aeaa9b5bcf2ef7f95eb8aadf9f20
Narongchai Krutcham <narongchai.k63@rsu.ac.th>
2023-02-20 15:36:29 +0700

    Add my name

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   return (
     <div>
       <MyBar name="Me App" />
-      <h1>Hello DIT, RSU</h1>
+      <h1>Narongchai Krutcham 6302037</h1>
       <Grid container spacing={2}>
       {data.map(item => (
         <Grid item xs={12} md={4}>
